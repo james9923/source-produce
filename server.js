9cab989af0de6b51cbf23c6e1d2a8b0ef2ef4d05
unknown <jaystar1311@gmail.com>
2020-08-18 22:07:06 -0700

    Implemented More CORS Policy

--- server.js
+++ server.js
@@ -13,7 +13,9 @@
 		const app = express();
 		app.use(cors());
 		app.use((req, res, next) => {
-  			res.header('Access-Control-Allow-Origin', '*');
+  			res.setHeader('Access-Control-Allow-Origin', '*');
+  			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type,Authorization');
  		     next();
 		});
 
